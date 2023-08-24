12789c4f05eee71c86395fed9013c89623172ef4
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-08-24 07:25:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st 
+from PIL import Image
 st.title("mi primer app")
 
-from PIL import image
+
 
 st.header("en este espacio desarrollo mi app")
 image= Image.open("nacional.png")
 
 st.image(image,caption ='nacional')
+
+texto= st.text_input("solo nacional a morir",texto)
