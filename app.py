891c318f5179b9c25cefca8473c62249ac59b5b8
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-08-24 07:09:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st 
 st.title("mi primer app")
+
+from PIL import image
+
+st.header("en este espacio desarrollo mi app")
+image= image.open("nacional.png")
+
+st.image(image,caption"nacional")
