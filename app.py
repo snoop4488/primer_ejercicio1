d48da7a7de4cc9f8aeec707baae99f97ab56f78b
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-08-24 07:21:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.title("mi primer app")
 from PIL import image
 
 st.header("en este espacio desarrollo mi app")
-image= image.open("nacional.png")
+image= Image.open("nacional.png")
 
 st.image(image,caption ='nacional')
